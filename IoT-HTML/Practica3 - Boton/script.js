2b6f09881f07b7aeccfe92828bb29e4a285f3492
JakeLegacyDECLA <A01712823@tec.mx>
2025-10-22 22:32:30 -0600

    Add files via upload

---
+++ IoT-HTML/Practica3 - Boton/script.js
@@ -0,0 +1,19 @@
+function evoFuego(event) {
+  event.preventDefault();
+  document.getElementById('Eevee').src = '../imagenes/Flareon.png';
+}
+
+function evoAgua(event) {
+  event.preventDefault();
+  document.getElementById('Eevee').src = '../imagenes/Vaporeon.png';
+}
+
+function evoRayo(event) {
+  event.preventDefault();
+  document.getElementById('Eevee').src = '../imagenes/Jolteon.png';
+}
+
+function Desevo(event) {
+  event.preventDefault();
+  document.getElementById('Eevee').src = '../imagenes/Eevee.png';
+}
